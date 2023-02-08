28997d324faaeafb5f331401338256e8e2626cd3
Muhammad Danil Elistz <123985221+Zeltoria@users.noreply.github.com>
2023-02-08 20:06:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+async function maubucin(){
+await swals.fire({
+  title: '<strong>Mau script bucin yang lain?</strong>',
+  html:
+    'Cek scriptnya di ' +
+    '<a href="https://bit.ly/htmlbucin2022">https://bit.ly/scripthtmlbucin</a> ' +
+    'ya!' + '<br><br>' + 'Bisa diedit sesuka hatimu menggunakan aplikasi Quick Editor ataupun sejenisnya.',
+  focusConfirm: false,
+});
+window.location = "https://bit.ly/htmlbucin2022";
+} 
+async function maubucin2(){
+await swals.fire({
+  title: '<strong>Mau mentahan script bucinnya?</strong>',
+  html:
+    'Ambil scriptnya di: ' +
+    '<a href="https://sfile.mobi/1KUBvuLh0k27">Klik di Sini!</a> ' +
+    'ya' + '<br><br>' + 'Bisa diedit sesukamu menggunakan aplikasi Quick Editor ataupun HTML Editor lainnya.',
+  focusConfirm: false,
+});
+window.location = "https://sfile.mobi/1KUBvuLh0k27";
+}
